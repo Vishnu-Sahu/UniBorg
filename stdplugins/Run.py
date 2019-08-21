261dd911d11a85feec1af9bb10c785506b628cb5
bugsbunnyuserbot <52664591+bugsbunnyuserbot@users.noreply.github.com>
2019-08-21 16:36:35 +0000

    Update Run.py

--- stdplugins/Run.py
+++ stdplugins/Run.py
@@ -10,7 +10,7 @@ from collections import deque
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🏃‍♂🏃‍♂🏃‍♂🏃‍♂🏃‍♂🏃‍♂🏃‍"))
+	deq = deque(list("🐿🐰🐿🐰🐇🐿🐇🐿🐇🐿🐇🐿🐰"))
 	for _ in range(48):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
