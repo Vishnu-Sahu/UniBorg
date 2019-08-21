58b6019321c6765eed3d56525f0cc7b06a48c0ed
bugsbunnyuserbot <52664591+bugsbunnyuserbot@users.noreply.github.com>
2019-08-21 16:25:39 +0000

    Update hack.py

--- stdplugins/hack.py
+++ stdplugins/hack.py
@@ -48,7 +48,7 @@ async def _(event):
             "`Loading... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Loading... 84%\n█████████████████████▒▒▒▒ `",
             "`Loading... 100%\n█████████████████████████ `",
-            "`Targeted Account Hacked...\n\nPay 100$ To @ViciPro To Remove This Hack`"
+            "`Targeted Account Hacked...\n\nPay 100$ To @iamkevinbest To Remove This Hack`"
         ]
 
         for i in animation_ttl:
