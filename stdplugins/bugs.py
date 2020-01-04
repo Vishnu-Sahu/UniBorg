321a3b0c88395bb0b8298414a214e8c27d02c7bf
bugsbunnyuserbot <52664591+bugsbunnyuserbot@users.noreply.github.com>
2020-01-04 11:51:37 +0000

    Update and rename Run(2).py to bugs.py

--- stdplugins/bugs.py
+++ stdplugins/bugs.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.run", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.bugs", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🏃‍♂🏃‍♂🏃‍♂🏃‍♂🏃‍♂🏃‍♂🏃‍"))
+	deq = deque(list("🐇🐰🐇🐰🐇🐰🐇🐰🐇🐰🐇🐰🐇"))
 	for _ in range(48):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
