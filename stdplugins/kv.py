754c8211507e880e1b112326ab71ed8b4dc35882
bugsbunnyuserbot <52664591+bugsbunnyuserbot@users.noreply.github.com>
2020-01-04 13:14:01 +0000

    Update and rename rain.py to kv.py

--- stdplugins/kv.py
+++ stdplugins/kv.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.rain", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.kv", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🌬☁️🌩🌨🌧🌦🌥⛅🌤"))
+	deq = deque(list("☆꧁༒☬🤖🐰ᵏєᵛ丨Ň🐰🤖☬༒꧂☆:"))
 	for _ in range(48):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
