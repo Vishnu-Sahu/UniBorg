7ba7b9b485a34d8460d84e65a4a2352c5247f468
bugsbunnyuserbot <52664591+bugsbunnyuserbot@users.noreply.github.com>
2020-01-04 18:11:34 +0000

    Update and rename thinklol.py to haha.py

--- stdplugins/haha.py
+++ stdplugins/haha.py
@@ -5,11 +5,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.lol", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.haha", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("🤔🧐🤔🧐🤔🧐"))
+	deq = deque(list("😬😆🥴😬🤣🤨"))
 	for _ in range(999):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
